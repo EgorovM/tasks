4591d3aab3aae2a17731ac94f5031e3114df88db
EgorovM <egorov_michil@mail.ru>
2018-12-03 21:58:08 +0900

    Update thirdtask.py

--- thirdtask.py
+++ thirdtask.py
@@ -1,8 +1,6 @@
 l = list(map(int,input().split()))
 
-s = 0
+s_now = sum(l)
+s_real = ((len(l)+1)*(len(l)+2))//2
 
-for i in range(len(l)):
-    s += l[i]
-
-print(((len(l)+1)*(len(l)+2))//2 - s)
+print( s_real - s_now )

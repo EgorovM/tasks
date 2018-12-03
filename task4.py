e220b3b1edd02f64702145475c03c9a9668f02cb
EgorovM <egorov_michil@mail.ru>
2018-12-03 22:36:42 +0900

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,17 +1,24 @@
+import math
+
 l = list(map(int,input().split()))
-l.sort()
-k = 0
-fl = True
+   
+s_real = ((len(l)+3)*(len(l)+2))//2
+d_real = 1
+s_now = 0
+d_now = 1
 
-for i in range(1,min(max(l)+1,len(l)+1)):
-    if l[i-1] != i + k:
-        print(i + k, end = " ")
-        k = k + 1
+for i in range(len(l)):
+    d_real *= i+1
+    d_now *= l[i]
+    s_now += l[i]
 
-        if k == 2:
-            break
+d_real *= ((len(l)+1) * (len(l)+2))
 
-if k == 1:
-    print(max(l)+1)
-elif k == 0:
-    print(max(l)+1,max(l)+2)
+
+s1 = s_real - s_now
+s2 = d_real // d_now
+
+n1 = int((s1 - math.sqrt(s1*s1 - 4*s2)) // 2)
+n2 = int(s1 - n1)
+
+print(n1 , n2)

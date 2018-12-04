70b6579db2083fa20ae6e02c31e236c2dd79cdc8
EgorovM <egorov_michil@mail.ru>
2018-12-04 22:14:59 +0900

    Update warmuptask.py

--- warmuptask.py
+++ warmuptask.py
@@ -3,27 +3,13 @@ import sys
 l = list(map(int,input().split()))
 s = int(input())
 
-overs = {}
+d = set()
 
 for i in range(len(l)):
-    overs[l[i]%s] = 0
+    if (s - l[i]) in d:
+        print("Yes")
+        sys.exit()
+        
+    d.add(l[i])
 
-for i in range(len(l)):
-    overs[l[i]%s] += 1
-
-ans = "No"
-
-if l[0] >= 2:
-    ans = "Yes"
-else:
-    for i in range(1,(s-1)//2):
-        if (l[i]%s in overs) and (s-(l[i]%s) in overs):
-            ans = "Yes"
-            break
-
-    if s % 2 == 0:
-        if s//2 in overs and overs[(s//2)] >= 2:
-            ans = "Yes"
-            
-print(ans)
-    
+print("No")
